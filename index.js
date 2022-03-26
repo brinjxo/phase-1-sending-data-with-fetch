251726edbb9cb0e339b6bc689918b7cc87830a36
Brindia Joseph <brindiatyjuana@gmail.com>
2022-03-26 15:58:27 -0500

    commit tree 7d439295711cd3bed6a113fdfe328c80e812b063

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName,userEmail){
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            userName,
+            userEmail
+          } )
+        } )
+   .then(function(resp){
+       return resp.json()
+   })
+   .then(function (object) {
+    document.body.innerHTML= object["id"]
+   })
+   .catch(function(error){
+       document.body.innerHTML= error.message
+   })
+};
